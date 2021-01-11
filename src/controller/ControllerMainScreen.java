2f8ed8ca596238bcdcc94eb359f8873ef53fa778
Rennan Silva Costa <rennansilvacosta@hotmail.com>
2021-01-10 21:13:11 -0300

    create mainScreen

--- src/controller/ControllerMainScreen.java
+++ src/controller/ControllerMainScreen.java
@@ -1,4 +1,45 @@
 package controller;
 
-public class ControllerMainScreen {
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerMainScreen implements Initializable {
+
+    @FXML
+    JFXButton btnAddDespesa, btnAddReceita = new JFXButton();
+
+    @FXML
+    Label txtTotalReceita, txtTotalDespesa, txtTotalSaldo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    public void openAddReceitaScreen() {
+        btnAddReceita.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Receita");
+            }
+        });
+    }
+
+    @FXML
+    public void openAddDespesaScreen() {
+        btnAddDespesa.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Despesa");
+            }
+        });
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,14 +3,33 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        scene.getRoot().requestFocus();
+
+        scene.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+            primaryStage.setOpacity(0.7);
+        });
+        scene.setOnMouseReleased(mouseEvent -> primaryStage.setOpacity(1));
     }
 
 
